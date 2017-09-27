41e0a9ae96bba3834f83d0d0aad571f1527377d1
Chris Kim <christopherjkim@gmail.com>
2017-09-27 18:02:47 -0400

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,41 +1,18 @@
 // #UNCOMMENTED_IN_PROD# import 'airbnb-js-shims/target/es2015';
 // #UNCOMMENTED_IN_PROD# import 'airbnb-browser-shims/browser-only';
 
-import Soe from './lib/soe';
 import initGame from './lib/game';
+import GeneratorDemo from './generator-demo';
 
-function generate(type) {
-  const listElem = document.getElementById('equation-list');
-  const pair = Soe.generateType(type);
-  const { x, y } = Soe.solve(pair);
-  listElem.innerText += `${pair[0].format()}\n${pair[1].format()}\n`;
-  listElem.innerText += `x = ${x}\ny = ${y}\n\n`;
-}
+const container = document.getElementById('container');
 
-function createButton(type) {
-  const container = document.getElementById('container');
-  const button = document.createElement('input');
-  container.appendChild(button);
-  button.type = 'button';
-  button.id = `generate-${type}-button`;
-  button.value = `generate ${type}`;
-  button.onclick = () => { generate(type); };
-  return button;
-}
-
-createButton('elimination');
-createButton('substitution');
-createButton('random');
-
-const list = document.createElement('pre');
-list.id = 'equation-list';
-document.getElementById('container').appendChild(list);
-
-generate('random');
+const generatorDemo = new GeneratorDemo();
+container.appendChild(generatorDemo.containerElem);
+generatorDemo.generate();
 
 const canvas = document.createElement('canvas');
 canvas.id = 'game';
 canvas.height = 999;
 canvas.width = 999;
-document.body.appendChild(canvas);
+container.appendChild(canvas);
 initGame(canvas);
---
+++ src/generator-demo.js
@@ -0,0 +1,49 @@
+import Soe from './lib/soe';
+
+function createElement(tagName = 'div', className = '') {
+  const elem = document.createElement(tagName);
+  elem.className = className;
+  return elem;
+}
+
+class GeneratorDemo {
+  constructor() {
+    const containerElem = createElement('div', 'generator-demo');
+    const buttonContainerElem = createElement('div', 'generator-demo-buttons');
+    const listContainerElem = createElement('div', 'generator-demo-equations-container');
+    const listElem = createElement('pre', 'generator-demo-equations');
+
+    containerElem.appendChild(buttonContainerElem);
+    buttonContainerElem.appendChild(this.createButton('elimination'));
+    buttonContainerElem.appendChild(this.createButton('substitution'));
+    buttonContainerElem.appendChild(this.createButton('random'));
+
+    containerElem.appendChild(listContainerElem);
+    listContainerElem.appendChild(listElem);
+
+    this.containerElem = containerElem;
+    this.listElem = listElem;
+  }
+
+  generate(type) {
+    const pair = Soe.generateType(type);
+    const { x, y } = Soe.solve(pair);
+    this.listElem.innerText += `${pair[0].format()}\n${pair[1].format()}\n`;
+    this.listElem.innerText += `x = ${x}\ny = ${y}\n\n`;
+  }
+
+  createButton(type) {
+    const button = createElement('input');
+    button.type = 'button';
+    button.className = `generate-equation-button generate-equation-button-${type}`;
+    button.value = type;
+    button.onclick = () => { this.generate(type); };
+    return button;
+  }
+
+  containerElem() {
+    return this.containerElem;
+  }
+}
+
+export default GeneratorDemo;
